0537c9112be089222c0e9b9255d3b00ec1def12e
Nikita Ivanov <49843824+Evgaizen@users.noreply.github.com>
2025-11-15 22:46:46 +0300

    refactor(navbar): change file structure

--- app/components/Navbar/Navbar.test.tsx
+++ app/components/Navbar/Navbar.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import { Navbar } from "./index";
+import { Navbar } from "./Navbar";
 
 const LINKS = [
   {
