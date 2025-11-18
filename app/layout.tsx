05e820fc6aec3b27ddf87a9bdb99c212fb1d6920
Nikita Ivanov <49843824+Evgaizen@users.noreply.github.com>
2025-11-18 22:02:25 +0300

    Add greetings section

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,28 +10,26 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body>
-        <main className="flex flex-col justify-center items-center">
-          <Navbar
-            links={[
-              {
-                text: "About",
-                url: "/about",
-              },
-              {
-                text: "Blog",
-                url: "/blog",
-              },
-            ]}
-            avatar={
-              <Avatar
-                href="/"
-                imageUrl="https://avatars.githubusercontent.com/u/49843824"
-                altText="Nikita Ivanov"
-              />
-            }
-          />
-          {children}
-        </main>
+        <Navbar
+          links={[
+            {
+              text: "About",
+              url: "/about",
+            },
+            {
+              text: "Blog",
+              url: "/blog",
+            },
+          ]}
+          avatar={
+            <Avatar
+              href="/"
+              imageUrl="https://avatars.githubusercontent.com/u/49843824"
+              altText="Nikita Ivanov"
+            />
+          }
+        />
+        {children}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 export default function Home() {
   return (
-    <div className="bg-neutral-50 w-screen h-screen flex flex-col">Home</div>
+    <main className="flex flex-col justify-center items-center">
+      <Greetings />
+    </main>
+  );
+}
+
+function Greetings() {
+  return (
+    <section className="px-110 py-10 bg-neutral-50 w-screen h-screen flex flex-col">
+      <h2 className="text-3xl mb-2 font-bold">Hey there. 👋</h2>
+      <p className="text-xl">I&apos;m Nikita. I work in the Yandex Company.</p>
+    </section>
   );
 }
